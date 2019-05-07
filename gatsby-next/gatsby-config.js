372558a82697536a4a1f4554070da20acc1bffd7
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2019-05-07 10:45:09 -0400

    Add gatsby-source-filesystem and gatsby-transformer-json

--- gatsby-next/gatsby-config.js
+++ gatsby-next/gatsby-config.js
@@ -13,6 +13,20 @@ module.exports = {
     // If you want to use styled components you should add the plugin here.
     // 'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'publications',
+        path: './src/publications/',
+      },
+    },
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/publications/',
+      },
+    },
   ],
   siteMetadata: {
     title: 'My page',
