0a08205706c79b038bc65b3dc2d23d6e8f8c0c26
Matt McCormick <matt.mccormick@kitware.com>
2022-01-28 16:32:47 -0500

    Add favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types'
+import { Helmet } from 'react-helmet';
 
 import Header from './Header'
 import Copyright from './Copyright';
@@ -27,6 +28,12 @@ const Layout = ({ children }) => {
         }}
       >
       <Header />
+      <Helmet>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest"/>
+      </Helmet>
         <main>{children}</main>
         <SnackbarContent message="The journal is undergoing rebirth as a sustainable, modern website. We appreciate your thoughts and suggestions." action={provideFeedback} />
         <footer style={{ paddingTop: 80 }}>
